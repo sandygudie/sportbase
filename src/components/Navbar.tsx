99a4094a14ebeccfee909d1d8c703100588590cc
sandygudie <goodnewssandy@gmail.com>
2023-08-13 20:48:07 +0100

    added load more

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -100,12 +100,12 @@ export default function Navbar() {
         </div>
         <div className="text-xs flex md:gap-2 justify-center items-center">
           {" "}
-          <Link className="hover:text-primary" href={"/"}>
+          {/* <Link className="hover:text-primary" href={"/"}>
             Sign In
           </Link>
-          <span>|</span>
+          <span>|</span> */}
           <Link className="hover:text-primary" href={"/"}>
-            Help
+            Help Center
           </Link>
         </div>
       </div>
